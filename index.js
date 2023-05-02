d61479460e0aff44e5728869625e4f050c5c8072
Jack Boettcher <jcboettcher@college.harvard.edu>
2023-05-02 00:43:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const numQuestions = questions.length;
+
+let currentQuestion = 0;
+const progressEl = document.getElementById("progress-bar");
+const scenarioEl = document.getElementById("scenario");
+const questionEl = document.getElementById("question");
+
+const setBtn = () => {
+  document.getElementById("next-btn").addEventListener("click", () => {
+    const selectedOption = document.querySelector('input[name="answer"]:checked');
+    if (!selectedOption) {
+        return;
+    }
+
+    const answer = selectedOption.value;
+    // const weights = questions[currentQuestion].weights[answer];
+
+
+    currentQuestion++;
+    if (currentQuestion == questions.length) {
+        progressEl.style.width = `${currentQuestion/numQuestions*100}%`
+        // alert("Thank you for completing the survey!");
+        return;
+    }
+    showQuestion();
+  });
+}
+
+const showQuestion = () => {
+  progressEl.style.width = `${currentQuestion/numQuestions*100}%`
+  scenarioEl.innerHTML = "Scenario "+(currentQuestion+1);
+  const question = questions[currentQuestion];
+  questionEl.innerText = question.question;
+  const answerContainer = document.getElementById("answer-container");
+  const numOptions = question.options.length;
+  let s = ""
+  for (let i = 1; i <= numOptions; i++) {
+    s += `
+      <div class="option-div">
+        <input type="radio" name="answer" value="${i-1}" id="option${i}"/>
+        <label for="option${i}" id="option${i}-label"></label>
+      </div>`;
+  }
+  answerContainer.innerHTML = s+`
+    <div id="btn-div">
+      <button id="next-btn">Next</button>
+    </div>
+  `
+  for (let i = 1; i <= numOptions; i++) {
+    const option = document.getElementById("option"+i+"-label");
+    option.innerText = question.options[i-1];
+  }
+  setBtn();
+}
+
+showQuestion();
+
---
+++ questions.js
@@ -0,0 +1,56 @@
+const questions = [
+  {
+      question: "You are an accountant at a large firm and you notice some \
+      discrepancies in the books. You confront your boss about them who tells \
+      you to keep your mouth shut about it if you don't want to get fired. \
+      What do you do?",
+      options: [
+        "Do as your boss says and sweep it under the rug even though you know it's wrong",
+        "Use the discrepancies as leverage against your boss",
+        "Stay silent but quit your job because you can't work at a crooked company",
+        "Report the discrepancies and risk losing your job because of it",
+      ],
+      weights: [
+        [1,0,0,0,0,0,0],
+        [2,0,0,0,0,0,0],
+        [3,0,0,0,0,0,0],
+        [4,0,0,0,0,0,0],
+      ]
+  },
+  {
+    question: "hi",
+    options: [
+      "1",
+    ]
+  },
+  {
+    question: "hi",
+    options: [
+      "1",
+    ]
+  },
+  {
+    question: "hi",
+    options: [
+      "1",
+    ]
+  },
+  {
+    question: "hi",
+    options: [
+      "1",
+    ]
+  },
+  {
+    question: "hi",
+    options: [
+      "1",
+    ]
+  },
+  {
+    question: "hi",
+    options: [
+      "1",
+    ]
+  },
+];
